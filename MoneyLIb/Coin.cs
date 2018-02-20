dc557e207222b8e2bc8fae4525c26c33c3216495
Johnson7, Ben A <ben.johnson7@msubillings.edu>
2018-02-20 10:17:18 -0700

    Fix for Money.CompareTo

---
+++ MoneyLIb/Coin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyLib
+{
+    public abstract class Coin : CirculatingMoney
+    {
+        // Fields
+        private double _thicknessInCM = 0;
+        private double _radiusInCM = 0;
+        private double _weightInGrams = 0;
+
+        // Properties
+        public double ThicknessInCM
+        {
+            get
+            {
+                return _thicknessInCM;
+            }
+            internal set
+            {
+                _thicknessInCM = value;
+            }
+        }
+
+        public double RadiusInCM
+        {
+            get
+            {
+                return _radiusInCM;
+            }
+            internal set
+            {
+                _radiusInCM = value;
+            }
+        }
+
+        // Constructors
+        public Coin(Currency currency, decimal denomination) : base(currency, denomination)
+        {
+
+        }
+    }
+}
---
+++ MoneyLIb/USBankNotes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyLib
+{
+    public class DollarBill : BankNote
+    {
+        public DollarBill() : base(Currency.GetCountryCurrency("USD"),1)
+        {
+
+        }
+    }
+
+    public class TwoDollarBill : BankNote
+    {
+        public TwoDollarBill() : base(Currency.GetCountryCurrency("USD"), 2)
+        {
+
+        }
+    }
+
+    public class FiveDollarBill : BankNote
+    {
+        public FiveDollarBill() : base(Currency.GetCountryCurrency("USD"), 5)
+        {
+
+        }
+    }
+
+    public class TenDollarBill : BankNote
+    {
+        public TenDollarBill() : base(Currency.GetCountryCurrency("USD"), 10)
+        {
+
+        }
+    }
+
+    public class TwentyDollarBill : BankNote
+    {
+        public TwentyDollarBill() : base(Currency.GetCountryCurrency("USD"), 20)
+        {
+            
+        }
+    }
+}
